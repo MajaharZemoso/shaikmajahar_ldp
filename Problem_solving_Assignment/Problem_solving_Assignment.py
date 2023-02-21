227c11449afc463a81280a575ae0467991750461
Shaik Majahar <shaik.majahar@zemosolabs.com>
2023-02-21 21:39:58 +0530

    Changed Naming convensions

--- Problem_solving_Assignment/Problem_solving_Assignment.py
+++ Problem_solving_Assignment/Problem_solving_Assignment.py
@@ -12,7 +12,13 @@ For these i can use hash functions where the key is the location and the value i
 
 """
     1. Take the location preference from each candidate
-    2. For each location i can use a hash function where the key is the location and the value is the candidates who prefer it
+    2. For each candidate data 
+        - For all the preferred locations
+            - If the location matches to any of the available locations
+                - Then map the name of the candidate to the location
+    3. Step 2 will continue for all the candidates data
+    4. It returns the data where the candidates are matched to their preferred locations
+
 
     Note: In Python for hasing we use Dictionary
             so in my code I have used a dictionary
@@ -42,72 +48,92 @@ locations = [
 
 # Solution
 
-def Hasing_preference_locations(locations,candidates):
+def hasingPreferenceLocations(locations,candidates):
 
     # initializing the empty dictionary for mapping
-    mapped_locations ={i:[] for i in locations}
+    mappedLocations = {i:[] for i in locations}
 
     # Iterating through each candidate data and mapping their location
     for name,preference in candidates.items():
 
         # as one candidate may have multiple locations preference
-        for i in preference:
+        for subPreference in preference:
 
-            if i in locations: # checking the location preference is their in our location list or not
+            if subPreference in locations: # checking the location preference is their in our location list or not
 
-                mapped_locations[i].append(name)
+                mappedLocations[subPreference].append(name)
     
     # returning the mapped locations
-    return mapped_locations
+    return mappedLocations
 
 # Driver Code
 
 # Taking the count of candidates from the user
-n=int(input("Enter count of candidates  "))
+countOfCandidates=int(input("Enter count of candidates  "))
 
 # List of locations available
-print(locations)
+print(*locations)
 
 # List to store the candidates data
 candidates = {}
 
 # Iterating for collecting the candidates data
-for i in range(n):
+for i in range(countOfCandidates):
 
 
     # Collecting the candidates data
-    candidates_data = list(input("Enter candidates data like name along with preferences ").split(" "))
-    candidates[candidates_data[0]] = list(candidates_data[1:])
+    candidatesData = list(input("Enter candidates data like name along with preferences ").split(" "))
+    candidates[candidatesData[0]] = list(candidatesData[1:])
 
 # Calling the function
-mapped_data=Hasing_preference_locations(locations,candidates)
+mappedData=hasingPreferenceLocations(locations,candidates)
 
 # display the results
-for i,j in mapped_data.items():
-    if len(j)!=0:
-        print(i,j)
+for location,listOfCandidates in mappedData.items():
+    if len(listOfCandidates)!=0:
+        print(location,"---",listOfCandidates)
+
+
+
+
+
+# Time Complexity
+# Iterating through each candidate - O(n)
+# Checking if the location preference is in the list of locations - O(m)
+# Appending the candidate name to the list of mapped candidates for the location - O(1)
+
+# Total time complexity will be O(n^2)
+
+
+# Space Complexity
+"""
+The size of the dictionary can be at most m, and for each key in the dictionary, 
+the maximum size of the value list can be n. 
+Therefore, the space complexity of the program is O(n * m).
+
+"""
 
 
 
 
 # Data for Testing
 """
-Majahar Delhi Mumbai Pune
-Hari Kolkata Delhi
-Benhur Pune
-Seenu Bangalore Pune
-Bhanu Chennai Hyderabad Delhi
+Majahar Delhi Hyderabad Telangana
+Harish Tripura Delhi Chennai
+Seenu Bangalore Chennai
+Teja Hyderabad Pune
+Bhanu Chennai
 
 """
 
 # Output
 """
-Bangalore ['Seenu']
-Chennai ['Bhanu']
-Delhi ['Majahar', 'Hari', 'Bhanu']
-Hyderabad ['Bhanu']
-Kolkata ['Hari']
-Mumbai ['Majahar']
-Pune ['Majahar', 'Benhur', 'Seenu']
+Bangalore --- ['Seenu']
+Chennai --- ['Harish', 'Seenu', 'Bhanu']
+Delhi --- ['Majahar', 'Harish']
+Hyderabad --- ['Majahar', 'Teja']
+Pune --- ['Teja']
+Telangana --- ['Majahar']
+Tripura --- ['Harish']
 
 """
